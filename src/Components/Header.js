146d0ffb32b0529cbb56d524acb3edecb0f48e50
Aastha Saxena <saxena.aastha91@gmail.com>
2023-12-22 10:37:59 -0500

    modified Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,21 @@ import React from 'react'
 export default function Header() {
   return (
     <div>
-      <h2> What would you like to do today?</h2>
+      <div class="container-xl">100% wide until extra large breakpoint
+
+        <div class="container px-4 text-center">
+          <div class="row gx-5">
+            <div class="col">
+
+              <button type="button" href="#" role="button" class="btn btn-danger btn-lg">Danger</button>
+            </div>
+            <div class="col">
+
+              <button type="button" href="#" role="button" class="btn btn-danger btn-lg">Danger</button>
+            </div>
+          </div>
+        </div></div>
+
     </div>
   )
 }
